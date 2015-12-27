71eafa97e6dfcddfffd0c0cba1b8cea97b8eb75a
Jeremy Beker <gothmog@confusticate.com>
2015-12-27 15:31:10 -0500

    New Project

---
+++ AOC_1/AOC_1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AOC_1
+//
+//  Created by Jeremy Beker on 12/27/15.
+//  Copyright © 2015 Confusticate. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
