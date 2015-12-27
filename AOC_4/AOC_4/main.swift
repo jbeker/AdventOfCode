81b44a0cc4801c61ec313c7d6940ce3e3e882436
Jeremy Beker <gothmog@confusticate.com>
2015-12-27 18:34:18 -0500

    Day 4 part 1

---
+++ AOC_4/AOC_4/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  AOC_4
+//
+//  Created by Jeremy Beker on 12/27/15.
+//  Copyright © 2015 Confusticate. All rights reserved.
+//
+
+import Foundation
+
+let key = "iwrupvqb"
+
+
+for i in 0..<Int.max {
+    let test = NSString(string: "\(key)\(i)")
+    let hash = test.MD5Digest()
+
+    if hash.substringToIndex(hash.startIndex.advancedBy(5)) == "00000" {
+        print("hash: \(hash) seed: \(test) index: \(i)")
+        break
+    }
+}
